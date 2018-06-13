4228d9f865382450c1af4a95d79dffaf57bb00aa
Ponlawith Annet <ponlawitaunn@gmail.com>
2018-06-13 13:02:08 +0200

    Add uåload files and dowload

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -32,5 +32,45 @@ export class MainComponent implements OnInit {
       })
     })
   }
-
+uploadFile() {
+  var fileInput = (<HTMLInputElement> document.getElementById('file-upload'));
+  var file = fileInput.files[0];
+  dbx.filesUpload({path: '/' + file.name, contents: file})
+    .then((response)=> {
+      console.log(response);
+      let url = this.router.url;
+      if (url == "/") {
+        url = ""
+      }
+      dbx.filesListFolder({path: url})
+      .then((response) => {
+        console.log(response);
+        this.entries = response.entries;
+      })
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  return false;
+}
+//downloadFile(entri.path_lower)
+downloadFile(entri) {
+  dbx.filesDownload({path: entri})
+    .then(function(data) {
+      // NOTE: The Dropbox SDK specification does not include a fileBlob
+      // field on the FileLinkMetadataReference type, so it is missing from
+      // the TypeScript type. This field is injected by the Dropbox SDK.
+      var downloadUrl = URL.createObjectURL((<any> data).fileBlob);
+      var downloadButton = document.createElement('a');
+      downloadButton.setAttribute('href', downloadUrl);
+      downloadButton.setAttribute('download', data.name);
+      downloadButton.setAttribute('class', 'button');
+      downloadButton.innerText = 'Download: ' + data.name;
+      downloadButton.click();
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  return false;
+}
 }
